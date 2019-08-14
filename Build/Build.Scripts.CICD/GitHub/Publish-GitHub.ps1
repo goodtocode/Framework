2ced99b03afcee54626986125106ad014079efb3
Robert Good <robert.good@goodtocode.com>
2019-08-13 21:30:32 -0700

    Sprint 8 - Publish-GitHub Script

---
+++ Build/Build.Scripts.CICD/GitHub/Publish-GitHub.ps1
@@ -0,0 +1,62 @@
+﻿#-----------------------------------------------------------------------
+# <copyright file="Publish-GitHub.ps1" company="GoodToCode">
+#      Copyright (c) GoodToCode. All rights reserved.
+#      All rights are reserved. Reproduction or transmission in whole or in part, in
+#      any form or by any means, electronic, mechanical or otherwise, is prohibited
+#      without the prior written consent of the copyright owner.
+# </copyright>
+#-----------------------------------------------------------------------
+
+# ***
+# *** Parameters
+# ***
+param(
+	[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+	[String]$ArtifactDir = $(throw '-ArtifactDir is a required parameter. c:\builds\1\a'),
+	[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+	[String]$TempDir = $(throw '-TempDir is a required parameter. c:\builds\1\t'),
+	[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+	[String]$Url = $(throw '-Url is a required parameter. https://github.com/goodtocode/Framework.git'),
+	[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+	[String]$RepoName = $(throw '-RepoName is a required parameter. Framework'),
+	[String]$User = 'Robert Good',
+	[String]$Email = 'robert.good@goodtocode.com'
+)
+
+# ***
+# *** Initialize
+# ***
+Set-ExecutionPolicy Unrestricted -Scope Process -Force
+$VerbosePreference = 'SilentlyContinue' # 'Continue'
+[String]$ThisScript = $MyInvocation.MyCommand.Path
+[String]$ThisDir = Split-Path $ThisScript
+Set-Location $ThisDir # Ensure our location is correct, so we can use relative paths
+Write-Host "*****************************"
+Write-Host "*** Starting: $ThisScript on $(Get-Date -format 'u')"
+Write-Host "*****************************"
+
+# ***
+# *** Locals
+# ***
+[String]$YearMo = (get-date).year.ToString()+'.'+(get-date).month.ToString("00")
+[String]$RepoDir = "$TempDir\$RepoName"
+
+# ***
+# *** Execute
+# ***
+If(-not (Test-Path -PathType Container -Path $ArtifactDir)) {New-Item -Path $ArtifactDir -ItemType directory -Force}
+If(-not (Test-Path -PathType Container -Path $TempDir)) {New-Item -Path $TempDir -ItemType directory -Force}
+Set-Location $TempDir
+& git config --global user.name $User
+& git config --global user.email $Email
+& git clone $Url
+If(-not (Test-Path -PathType Container -Path $RepoDir)) {New-Item -Path "$TempDir\$RepoName" -ItemType directory -Force}
+Set-Location $RepoDir
+Get-ChildItem -Path $ArtifactDir | % { 
+  Copy-Item $_.fullname "$RepoDir" -Recurse -Force 
+}
+& git add .
+& git commit -m "Iteration $YearMo"
+#git push -u origin master
+
+
---
+++ Build/Build.Scripts.CICD/_Tests/PS1-Publish-GitHub.ps1
@@ -0,0 +1,46 @@
+﻿#-----------------------------------------------------------------------
+# <copyright file="Framework-NuGet.ps1" company="GoodToCode">
+#      Copyright (c) GoodToCode. All rights reserved.
+#      All rights are reserved. Reproduction or transmission in whole or in part, in
+#      any form or by any means, electronic, mechanical or otherwise, is prohibited
+#      without the prior written consent of the copyright owner.
+# </copyright>
+#-----------------------------------------------------------------------
+
+# ***
+# *** Parameters
+# ***
+param(
+	[String]$TempDir = 'C:\Artifacts\t',
+	[String]$ArtifactDir = 'C:\Artifacts\a',
+	[String]$RepoName = 'Framework',
+	[String]$Url = 'https://github.com/goodtocode/Framework.git'
+)
+
+# ***
+# *** Initialize
+# ***
+Set-ExecutionPolicy Unrestricted -Scope Process -Force
+$VerbosePreference = 'Continue' # 'SilentlyContinue'
+[String]$ThisScript = $MyInvocation.MyCommand.Path
+[String]$ThisDir = Split-Path $ThisScript
+[DateTime]$Now = Get-Date
+Set-Location $ThisDir # Ensure our location is correct, so we can use relative paths
+Write-Host "*****************************"
+Write-Host "*** Starting: $ThisScript on $Now"
+Write-Host "*****************************"
+# Imports
+
+# ***
+# *** Validate and cleanse
+# ***
+
+# ***
+# *** Locals
+# ***
+
+# ***
+# *** Execute
+# ***
+# Publish-Vsix
+& "..\GitHub\Publish-GitHub.ps1" -ArtifactDir $ArtifactDir -TempDir $TempDir -Url $Url -RepoName $RepoName
