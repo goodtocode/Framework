f6f95de6343d62fe5a0941b76b00d2c0fb96a7b1
GoodToCode <robert.good@goodtocode.com>
2019-08-20 19:16:41 -0700

    Sprint 8 - Missed GRANT CONNECT to user for connection string to work

--- Quick-Starts/Framework.Database/Security/RoleMemberships.sql
+++ Quick-Starts/Framework.Database/Security/RoleMemberships.sql
@@ -7,5 +7,6 @@ ALTER ROLE [db_datawriter] ADD MEMBER [TestUser];
 GO
 ALTER ROLE [db_executor] ADD MEMBER [TestUser];
 GO
-GRANT CONNECT TO [testuser]
+GRANT CONNECT TO [TestUser];
 GO
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
